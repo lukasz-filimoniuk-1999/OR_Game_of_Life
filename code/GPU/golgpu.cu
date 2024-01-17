bbc402f155e6b46a6e5075f40694ab60949348f2
IgoreSHik <balyko070699@gmail.com>
2024-01-17 10:58:32 +0100

    GPU clock fix

--- code/GPU/golgpu.cu
+++ code/GPU/golgpu.cu
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
 
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 
-    printf("Execution time: %5.3f msecs\n", duration.count());
+    printf("Execution time: %d msecs\n", duration.count());
 
     WritePGM("final.pgm", imgSize, (stepLimit-1)%2);
 
